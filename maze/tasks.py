b9e19a01ccef4055486e83bfd4b92a4f0ca41366
Sepehr Norouzi <s.norouzi@medrick.info>
2025-10-06 15:30:55 +0330

    minor fixes

--- maze/tasks.py
+++ maze/tasks.py
@@ -2,12 +2,12 @@ from celery import shared_task
 from django.utils import timezone
 from django.core.files.base import ContentFile
 
-from maze.utils import maze_to_svg
 
 @shared_task
 def generate_maze_image(maze_id):
-    # return
     from maze.models import Maze
+    from maze.utils import maze_to_svg
+
     maze = Maze.objects.get(pk=maze_id)
     svg = maze_to_svg(maze.get_maze_binary())
     filename = f"maze_{maze.creator}_{timezone.now().strftime('%Y%m%d%H%M%S')}.svg"
@@ -16,6 +16,8 @@ def generate_maze_image(maze_id):
 @shared_task
 def generate_solved_maze_image(maze_id):
     from maze.models import Maze
+    from maze.utils import maze_to_svg
+
     maze = Maze.objects.get(pk=maze_id)
     svg = maze_to_svg(maze.get_solved_maze())
     filename = f"solved_maze_{maze.creator}_{timezone.now().strftime('%Y%m%d%H%M%S')}.svg"
--- maze/utils.py
+++ maze/utils.py
@@ -1,14 +1,15 @@
 import time
 from typing import List, Optional, Tuple
 
-from maze_runner.cpp_build import maze_generator
 
 
 def create_maze(size, mode, hybrid_prob=0.5):
+    from maze_runner.cpp_build import maze_generator
     return maze_generator.generate_maze(size, mode, hybrid_prob)
 
 
 def solve_maze(grid):
+    from maze_runner.cpp_build import maze_generator
     _t0 = time.time_ns()
     g = maze_generator.solve_maze(grid)
     _t1 = time.time_ns()
